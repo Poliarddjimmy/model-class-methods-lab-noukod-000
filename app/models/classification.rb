e1e5a0ea69a3f320cb8f20ab57e6293cb8dc36bf
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-30 14:20:44 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,16 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+
+  
+  def self.my_all
+  end
+
+  def self.my_all
+  end
+
+  def longest
+  end
+  
 end
